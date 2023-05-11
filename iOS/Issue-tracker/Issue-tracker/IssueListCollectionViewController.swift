1819bf445c7274d6b5321b73c5e4ae19a9855dce
Haena <ghkwlsspdl@naver.com>
2023-05-11 23:07:26 +0900

    feat: 셀간의 간격 조정 및 셀 배경색상 임시 조정
    가독성을 위한 셀 컬러 변경
    셀 간격을 0으로 세팅
    
    이슈: #6

--- iOS/Issue-tracker/Issue-tracker/IssueListCollectionViewController.swift
+++ iOS/Issue-tracker/Issue-tracker/IssueListCollectionViewController.swift
@@ -10,32 +10,33 @@ import UIKit
 class IssueListCollectionViewController: UIViewController {
 
     var collectionView: UICollectionView!
-    let cellRatio : CGFloat = 150/375
+    let cellRatio: CGFloat = 150/375
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let width = self.view.bounds.width
         let layout = initLayout()
+        layout.minimumLineSpacing = 0
         layout.itemSize = CGSize(width: width, height: width * cellRatio)
         collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
-        
+     
         setUI()
-        
+   
     }
     func setUI() {
         collectionView.delegate = self
         collectionView.dataSource = self
-        
+     
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-        collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "header")
+        collectionView.register(CollectionHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "header")
         self.view.addSubview(collectionView)
     }
 }
 
-extension IssueListCollectionViewController : UICollectionViewDelegate {
+extension IssueListCollectionViewController: UICollectionViewDelegate {
 }
 
-extension IssueListCollectionViewController : UICollectionViewDataSource {
+extension IssueListCollectionViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         //test를 위한 리턴값 100
         return 100
@@ -45,20 +46,25 @@ extension IssueListCollectionViewController : UICollectionViewDataSource {
         return 1
     }
 
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath as IndexPath)
-        cell.backgroundColor = UIColor.blue
+       if indexPath.row % 2 == 0{
+           cell.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.3)
+       }else{
+           cell.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.5)
+       }
         return cell
     }
 
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        
-        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath)
+   func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+       
+       guard let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath)
+              as? CollectionHeaderView else {return UICollectionReusableView()}
         return headerView
     }
-   
+  
 
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize{
+   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize{
         let width = self.view.frame.width
         let headerRatio = 94/375
         return CGSize(width: width, height: width * 94/375)
