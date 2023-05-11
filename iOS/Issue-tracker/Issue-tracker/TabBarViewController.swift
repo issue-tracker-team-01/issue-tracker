9cc925bad64719bfbcb3721115098c350b313274
Haena <ghkwlsspdl@naver.com>
2023-05-11 13:42:25 +0900

    feat: 탭바 아이템 구성
    탭바 아이템을 네개(이슈, 레이블, 마일스톤, 내계정)로 구성
    탭바 아이템 타이틀 변경
    탭바를 initViewController로 설정
    
    이슈: #4

--- iOS/Issue-tracker/Issue-tracker/TabBarViewController.swift
+++ iOS/Issue-tracker/Issue-tracker/TabBarViewController.swift
@@ -1,6 +1,6 @@
 //
 //  TabbarViewController.swift
-//  
+//
 //
 //  Created by leehwajin on 2023/05/11.
 //
@@ -11,6 +11,24 @@ class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        makeUI()
+
+    }
+
+    func makeUI() {
+        let issueViewController = UIViewController()
+        let labelViewController = UIViewController()
+        let milestoneViewController = UIViewController()
+        let myAccountVieController = UIViewController()
+
+        issueViewController.title = "이슈"
+        labelViewController.title = "레이블"
+        milestoneViewController.title = "마일스톤"
+        myAccountVieController.title = "내계정"
+
+        self.setViewControllers([issueViewController, labelViewController, milestoneViewController, myAccountVieController], animated: false)
+        self.modalPresentationStyle = .fullScreen
+        self.tabBar.backgroundColor = .white
 
     }
 
