8e1ee28c0c9f278e84df9dece693a25fafd3beec
Haena <ghkwlsspdl@naver.com>
2023-05-11 13:28:43 +0900

    feat: TabBarViewController 파일 생성
    여러 씬 간의 화면이동을 원활하게 하기 위한 탭바컨트롤러 생성
    
    이슈: #4

---
+++ iOS/Issue-tracker/Issue-tracker/TabBarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TabbarViewController.swift
+//  
+//
+//  Created by leehwajin on 2023/05/11.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
