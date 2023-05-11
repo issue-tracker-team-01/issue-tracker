ddd4d259fbe56d9541bdbbd395c36b10e67fdb11
Haena <ghkwlsspdl@naver.com>
2023-05-11 15:14:49 +0900

    feat: IssueListCOllectionViewController파일 추가
    이슈 탭에 있는 뷰컨트롤러 베이스를 컬렉션뷰로 추가하기 위한 파일 생성
    탭바 이슈탭과 연결
    
    이슈: #6

---
+++ iOS/Issue-tracker/Issue-tracker/IssueListCollectionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  IssueListCollectionViewController.swift
+//  Issue-tracker
+//
+//  Created by leehwajin on 2023/05/11.
+//
+
+import UIKit
+
+class IssueListCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
