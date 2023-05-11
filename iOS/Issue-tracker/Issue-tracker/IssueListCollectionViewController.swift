3bb01857c843d91dc4f02dd28c69cf6e205faa28
Haena <ghkwlsspdl@naver.com>
2023-05-11 16:10:00 +0900

    feat: 컬렉션뷰 레이아웃 및 셀 구현
    임시 셀을 추가하여 화면에 띄울 수 있도록 collectionViewDelegate, CollectionViewDataSource 채택 및 기본 설정
    셀 레이아웃을 위한 UICollectionViewDelegateFlowLayout 채택
    
    이슈: #6

--- iOS/Issue-tracker/Issue-tracker/IssueListCollectionViewController.swift
+++ iOS/Issue-tracker/Issue-tracker/IssueListCollectionViewController.swift
@@ -9,7 +9,42 @@ import UIKit
 
 class IssueListCollectionViewController: UIViewController {
 
+    var collectionView: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setup()
+
+    }
+    func setup() {
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout())
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.backgroundColor = UIColor.white
+        self.view.addSubview(collectionView)
+    }
+}
+
+extension IssueListCollectionViewController : UICollectionViewDelegate {
+}
+
+extension IssueListCollectionViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //test를 위한 리턴값 10
+        return 100
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath as IndexPath)
+        cell.backgroundColor = UIColor.blue
+        return cell
+    }
+}
+
+extension IssueListCollectionViewController : UICollectionViewDelegateFlowLayout {
+    func layout() -> UICollectionViewFlowLayout {
+        return UICollectionViewFlowLayout()
     }
 }
