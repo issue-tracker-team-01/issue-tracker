4d5a5e20d62460923a167390640858c6e169ae68
harimrim <kigkfla@naver.com>
2023-05-12 11:17:46 +0900

    feat: milestones 구현
    이슈 목록의 마일스톤을 UILabel로 구현
    UILabel 내부에 UIImage를 넣어서 함께 표시하는 방식으로 구현하였으며 현재는 테스트 이미지가 보여집니다
    
    이슈: #5

--- iOS/Issue-tracker/Issue-tracker/IssueCell.swift
+++ iOS/Issue-tracker/Issue-tracker/IssueCell.swift
@@ -23,4 +23,16 @@ class IssueCell: UICollectionViewCell {
         lbl.text = "asdfadfasdffaddfadfsadsfasdggggasgadgggsdsdadfgkbjlasdfgasdghjkgsdhjkgahjkasdghjklasdghjkladggasdhjkl"
         return lbl
     }()
+    private let milestones: UILabel = {
+        let lbl = UILabel()
+        lbl.font = .systemFont(ofSize: 22)
+        let attributedString = NSMutableAttributedString(string: "")
+        let imageAttachment = NSTextAttachment()
+        imageAttachment.image = UIImage(systemName: "arrowshape.right")
+        attributedString.append(NSAttributedString(attachment: imageAttachment))
+        imageAttachment.bounds = CGRect(x: 0, y: 0, width: 18, height: 18)
+        attributedString.append(NSAttributedString(string: "test"))
+        lbl.attributedText = attributedString
+        return lbl
+    }()
 }
