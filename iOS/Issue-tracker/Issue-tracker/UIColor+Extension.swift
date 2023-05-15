584ae7756218fbb0d0b6f7ed0ed21b4f02f3f257
Haena <ghkwlsspdl@naver.com>
2023-05-15 11:48:25 +0900

    design: Extension+UIColor파일 세팅
    등록했었던 색상을 보다 간편히 사용하기 위하여 UICOlor을 확장
    
    이슈: #29

---
+++ iOS/Issue-tracker/Issue-tracker/UIColor+Extension.swift
@@ -0,0 +1,28 @@
+//
+//  UIColor + Extension.swift
+//  Issue-tracker
+//
+//  Created by leehwajin on 2023/05/15.
+//
+
+import UIKit
+
+extension UIColor{
+    static let neutralTextWeak = UIColor(named: "neutral.text-weak")
+    static let neutralText = UIColor(named: "neutral.text")
+    static let neutralTextStrong = UIColor(named: "neutral.text-strong")
+    static let neutralBackground = UIColor(named: "neutral.backgroud")
+    static let neutralBackgroundStrong = UIColor(named: "neutral.background-strong")
+    static let neutralBackgroundDisabled = UIColor(named: "neutral.background-disabled")
+    static let neutralBorder = UIColor(named: "neutral.border")
+    static let netralBoorderStrong = UIColor(named: "neutral.border-strong")
+    static let accentTextStrong = UIColor(named: "accent.text-strong")
+    static let accentTextPrimary = UIColor(named: "accent.text-primary")
+    static let accentTextSecondary = UIColor(named: "accent.text-secondary")
+    static let accentBackgroundStrong = UIColor(named: "accent.background-strong")
+    static let accentBackgroundStrongPrimary = UIColor(named: "accent.background-strong-primary")
+    static let accentBackgroundStrongSecondary = UIColor(named: "accetn.background-strong-secondary")
+    static let dangerText = UIColor(named: "danger.text")
+    static let dangerTextStrong = UIColor(named: "danger.text-strong")
+    static let dangerBackgroundStrong = UIColor(named:"danger.background-strong")
+}
