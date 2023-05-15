ca7d1ddae802d95e33d05b908aca8bf21e30ecfa
harimrim <kigkfla@naver.com>
2023-05-15 16:09:47 +0900

    feat: tagLabel 구현
    이슈 목록 내부의 태그를 TagListView 라이브러리를 사용하여 코드로 구현
    
    이슈: #5

--- iOS/Issue-tracker/Issue-tracker/IssueCell.swift
+++ iOS/Issue-tracker/Issue-tracker/IssueCell.swift
@@ -37,24 +37,35 @@ class IssueCell: UICollectionViewCell {
         lbl.attributedText = attributedString
         return lbl
     }()
-    
+    private let tagLabel: TagListView = {
+        let tags = TagListView()
+        tags.bounds = CGRect(x: 0, y: 0, width: 335, height: 18)
+        tags.addTags(["Add", "two", "tags"])
+        tags.insertTag("This should be the second tag", at: 1)
+        tags.alignment = .leading
+        tags.textFont = UIFont.systemFont(ofSize: 12)
+        return tags
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(titleLabel)
         addSubview(descriptionLabel)
         addSubview(milestones)
+        addSubview(tagLabel)
         milestones.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-        
+        tagLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 16).isActive = true
         titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 24).isActive = true
         descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10).isActive = true
         descriptionLabel.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
         descriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10).isActive = true
         milestones.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 10).isActive = true
-
+        tagLabel.topAnchor.constraint(equalTo: milestones.bottomAnchor, constant: 10).isActive = true
+        tagLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10).isActive = true
+        
     }
     required init?(coder: NSCoder) {
         fatalError("ERROR")
