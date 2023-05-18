05cad8d4d8fd48777b882f16e438301a1ade805a
Haena <ghkwlsspdl@naver.com>
2023-05-19 05:19:39 +0900

    feat: 테이블뷰 생성 및 레이아웃
    테이블뷰를 생성하고 레이아웃 해줌으로써 뷰컨트롤러의 뷰와 같은 크기로 올라감
    
    이슈: #51

--- iOS/Issue-tracker/Issue-tracker/FilterIssueTable/FilterTableViewController.swift
+++ iOS/Issue-tracker/Issue-tracker/FilterIssueTable/FilterTableViewController.swift
@@ -9,21 +9,29 @@ import UIKit
 
 class FilterTableViewController: UIViewController {
 
+    let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableViewLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableViewLayout() {
+        tableView.rowHeight = 44
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.register(UITableViewHeaderFooterView.self, forHeaderFooterViewReuseIdentifier: "header")
+        
+        view.addSubview(tableView)
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
     }
-    */
 
 }
