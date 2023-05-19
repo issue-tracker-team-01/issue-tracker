900bdd2a1a362cfca527e6acab8ec64700a67220
poco <godiva140@gmail.com>
2023-05-19 11:13:06 +0900

    feat: msw 설정 파일 추가 및 mock 데이터 추가
    msw 설정 파일 추가 및 mock 데이터 추가
    
    Related to # 49

--- fe/src/index.js
+++ fe/src/index.js
@@ -4,6 +4,11 @@ import { ThemeProvider } from 'styled-components';
 import App from './App';
 import GlobalStyle from './styles/GlobalStyle';
 import THEME from './styles/theme';
+import { worker } from './mocks/worker';
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
