5f709318b040911324d51c56cdaae3a9fa6d77f7
poco <godiva140@gmail.com>
2023-05-24 20:46:03 +0900

    feat: 이슈 작성 버튼 클릭시 페이지 이동
    새로운 이슈 작성 페이지로 이동
    
    Related to #85

--- fe/src/components/mainPage/nav/Nav.jsx
+++ fe/src/components/mainPage/nav/Nav.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Button from '../../../components/common/button/Button';
 import PlusIcon from '../../../assets/icons/PlusIcon.svg';
 import Tab from '../../../components/common/tabs/Tabs';
+import { Link } from 'react-router-dom';
 
 const NavContainer = styled.div`
   display: flex;
@@ -23,10 +24,12 @@ const Nav = () => {
       <FilterBar />
       <SubNavBlock>
         <Tab></Tab>
-        <Button type="container" size="medium">
-          <img src={PlusIcon} />
-          이슈 작성
-        </Button>
+        <Link to="/newIssue">
+          <Button type="container" size="medium">
+            <img src={PlusIcon} />
+            이슈 작성
+          </Button>
+        </Link>
       </SubNavBlock>
     </NavContainer>
   );
