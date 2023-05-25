3f493608e71f1680d1d0d60bd21c5fa90f9085fd
Haena <ghkwlsspdl@naver.com>
2023-05-25 13:25:30 +0900

    feat: CustomNaviFilter 파일 생성
    필터UI의 네비게이션바를 커스텀 할 파일 생성
    
    이슈: #51

---
+++ iOS/Issue-tracker/Issue-tracker/FilterIssueTable/CustomNaviFilter.swift
@@ -0,0 +1,20 @@
+//
+//  CustomNaviFilter.swift
+//  Issue-tracker
+//
+//  Created by leehwajin on 2023/05/25.
+//
+
+import UIKit
+
+class CustomNaviFilter: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
