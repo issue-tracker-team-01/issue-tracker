1949188e54eb9a57ba2d75443d0e55f51313404e
saseung <seung0122@naver.com>
2023-05-25 16:05:22 +0900

    feat: 드롭다운 컴포넌트 비동기 처리
    - 드롭다운 비동기 처리
    - 드롭다운 기능 이벤트 처리
    
    related to #75 #76

---
+++ fe/src/components/common/dropdown/Dropdown.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect, useRef } from 'react';
+import styled from 'styled-components';
+import DropdownButton from './dropdownContent/DropdownButton';
+import DropdownPanel from './dropdownContent/DropdownPanel';
+
+const DropdownContainer = styled.div`
+  position: relative;
+`;
+
+const Dropdown = ({ title, fetchDropdownContent }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [dropdownContent, setDropdownContent] = useState([]);
+  const ref = useRef();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchDropdownContent();
+      setDropdownContent(data);
+    };
+
+    if (isOpen) {
+      fetchData();
+    }
+  }, [isOpen, fetchDropdownContent]);
+
+  const handleClickOutside = (event) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const handleToggleDropdown = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
+  return (
+    <DropdownContainer ref={ref}>
+      <DropdownButton buttonName={title} onClick={handleToggleDropdown} />
+      {isOpen && <DropdownPanel content={dropdownContent} />}
+    </DropdownContainer>
+  );
+};
+
+export default Dropdown;
