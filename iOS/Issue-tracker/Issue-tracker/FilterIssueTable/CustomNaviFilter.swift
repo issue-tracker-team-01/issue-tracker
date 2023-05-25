5919748d3498f7c95d71af6232908c6f87ac02e3
Haena <ghkwlsspdl@naver.com>
2023-05-25 13:27:09 +0900

    feat: cancleButton생성
    커프텀네비필터에서 쓸 cancelButton생성 및 속성 구현
    
    이슈: #51

--- iOS/Issue-tracker/Issue-tracker/FilterIssueTable/CustomNaviFilter.swift
+++ iOS/Issue-tracker/Issue-tracker/FilterIssueTable/CustomNaviFilter.swift
@@ -8,13 +8,12 @@
 import UIKit
 
 class CustomNaviFilter: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+    
+    private lazy var cancelButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("< 취소", for: .normal)
+        button.setTitleColor(UIColor(red: 0, green: 122/255, blue: 255/255, alpha: 1), for: .normal)
+        button.titleLabel?.font = UIFont(name: "SFProDisplay-Semibold", size: 18)
+        return button
+    }()
 }
