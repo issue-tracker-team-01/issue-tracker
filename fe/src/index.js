093a39c2d0180373fee602f1d7be040a4537919a
poco <godiva140@gmail.com>
2023-05-26 11:38:42 +0900

    style: msw 사용 코드 삭제

--- fe/src/index.js
+++ fe/src/index.js
@@ -4,12 +4,7 @@ import { ThemeProvider } from 'styled-components';
 import App from './App';
 import GlobalStyle from './styles/GlobalStyle';
 import THEME from './styles/theme';
-import { worker } from './mocks/worker';
 import { BrowserRouter } from 'react-router-dom';
-
-if (process.env.NODE_ENV === 'development') {
-  worker.start();
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
