5f3c1c958eaff09ea1c0835f839f7b0d430377bb
saseung <seung0122@naver.com>
2023-05-26 11:38:06 +0900

    design: 드롭다운 스타일 추가 작업
    - 드롭다운 패널 리스트와 이미지 스타일 추가 작업
    - 체크 버튼 svg 파일 추가
    
    related to #97

--- fe/src/components/mainPage/issueTable/header/issueFilterOptions/IssueFilterOptions.jsx
+++ fe/src/components/mainPage/issueTable/header/issueFilterOptions/IssueFilterOptions.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Dropdown from '../../../../common/dropdown/Dropdown';
+import apiUrl from '../../../../../utils/api/api';
 
 const IssueFilterOptionsBox = styled.div`
   display: flex;
@@ -11,23 +12,24 @@ const IssueFilterOptionsBox = styled.div`
 
 const IssueFilterOptions = () => {
   const fetchDropdownContent = async (buttonType) => {
-    const response = await fetch(`/api/${buttonType}`);
+    const response = await fetch(`${apiUrl}/${buttonType}`);
     const data = await response.json();
 
     return data;
   };
 
   const issueDropdownButtons = [
-    { buttonName: '담당자', id: 'assignees' },
-    { buttonName: '레이블', id: 'labels' },
-    { buttonName: '마일스톤', id: 'milestones' },
-    { buttonName: '작성자', id: 'writers' },
+    { buttonName: '담당자', id: 'assignees', key: 'assignees' },
+    { buttonName: '레이블', id: 'labels', key: 'labels' },
+    { buttonName: '마일스톤', id: 'milestones?status=open', key: 'milestones' },
+    { buttonName: '작성자', id: 'writers', key: 'writers' },
   ];
   return (
     <IssueFilterOptionsBox>
       {issueDropdownButtons.map((button) => (
         <Dropdown
-          key={button.id}
+          key={button.key}
+          contentKey={button.key}
           title={button.buttonName}
           fetchDropdownContent={() => fetchDropdownContent(button.id)}
         />
