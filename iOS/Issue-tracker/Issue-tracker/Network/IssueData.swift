b7867e13de4bd26045aa693c13021e1ca584a5b8
Haena <ghkwlsspdl@naver.com>
2023-05-30 20:55:21 +0900

    feat: DTO구조 변경
    api로 받아오는 데이터명 중복으로 인해 네스트타입으로 변경
    
    이슈: #112

--- iOS/Issue-tracker/Issue-tracker/Network/IssueData.swift
+++ iOS/Issue-tracker/Issue-tracker/Network/IssueData.swift
@@ -7,17 +7,31 @@
 
 import Foundation
 
-struct IssueData: Codable {
-    let issues: [Issue]
+struct IssueList {
+    struct IssueData: Codable {
+        let issues: [Issue]
+    }
+    
+    struct Label: Codable {
+        let title: String?
+        let bgColorCode: String?
+    }
+    
+    struct Issue: Codable {
+        let title: String?
+        let labels: [Label]
+        let milestone: String?
+    }
 }
-
-struct Label: Codable {
-    let title: String?
-    let bgColorCode: String?
-}
-
-struct Issue: Codable {
-    let title: String?
-    let labels: [Label]
-    let milestone: String?
+struct LabelList {
+    // MARK: - Welcome10
+    struct LabelData {
+        let labels: [Label]
+    }
+    
+    // MARK: - Label
+    struct Label {
+        let id: Int
+        let title, labelDescription, bgColorCode, fontColorCode: String
+    }
 }
