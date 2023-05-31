405369a727e283b4370f6f848c91c9e890bd839c
saseung <seung0122@naver.com>
2023-05-31 17:38:22 +0900

    feat: 이슈상세페이지 header 구현
    - header 제목편집 누르면 상태 변경처리 완료
    - 헤더 레이아웃 스타일 및 컴포넌트 작업완료
    
    related to #115

---
+++ fe/src/components/issueDetail/detailHeader/index.jsx
@@ -0,0 +1,86 @@
+import { react, useState } from 'react';
+import {
+  ControlButtonBox,
+  IssueNumberBox,
+  IssueTitleBox,
+  IssueInfoBox,
+  LayoutStyle,
+  Line,
+} from './style';
+import Button from '@components/common/button';
+import EditIcon from '@assets/icons/EditIcon.svg';
+import archiveIcon from '@assets/icons/archiveIcon.svg';
+import Label from '@components/common/lable';
+import OpenIssueStatusIcon from '@assets/icons/OpenIssueStatusIcon.svg';
+import InputText from '@components/common/inputText';
+
+const DetailHeader = () => {
+  const [isEdit, setEdit] = useState(false);
+  const [input, setInput] = useState('');
+
+  const handleClick = () => {
+    setEdit(!isEdit);
+  };
+
+  const handleUpload = () => {
+    setInput(e.target.valut);
+  };
+
+  return (
+    <>
+      <LayoutStyle>
+        {!isEdit ? (
+          <>
+            <IssueTitleBox>
+              <span>input</span>
+              <IssueNumberBox>#2</IssueNumberBox>
+            </IssueTitleBox>
+            <ControlButtonBox>
+              <Button
+                type="outline"
+                size="medium"
+                icon={EditIcon}
+                title="제목 편집"
+                onClick={handleClick}
+              />
+              <Button type="outline" size="medium" icon={archiveIcon} title="이슈 닫기" />
+            </ControlButtonBox>
+          </>
+        ) : (
+          // input 컴포넌트 불러오기 (edit 클릭하면 input 컴포넌트로 변경)
+          <>
+            <InputText value={input}></InputText>
+            <ControlButtonBox>
+              <Button
+                type="outline"
+                size="medium"
+                icon={EditIcon}
+                title="편집 취소"
+                onClick={handleClick}
+              />
+              <Button
+                type="container"
+                size="medium"
+                icon={EditIcon}
+                title="편집 완료"
+                onClick={handleClick}
+              />
+            </ControlButtonBox>
+          </>
+        )}
+      </LayoutStyle>
+
+      <IssueInfoBox>
+        <Label
+          title="열린 이슈"
+          bgColor={({ theme }) => theme.COLOR.BLUE}
+          icon={OpenIssueStatusIcon}
+        />
+        <span>이 이슈가 3분전에 열렸음</span>
+      </IssueInfoBox>
+      <Line />
+    </>
+  );
+};
+
+export default DetailHeader;
