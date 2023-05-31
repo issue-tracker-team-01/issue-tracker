f22bf5390f24e22dbe535dba530a8deb9cc79c95
saseung <seung0122@naver.com>
2023-05-31 17:44:57 +0900

    feat: 상세페이지 콘텐츠 컴포넌트 및 레이아웃 작업
    - 상세페이지 컴포넌트 및 레이아웃 작업

---
+++ fe/src/components/issueDetail/detailContent/index.jsx
@@ -0,0 +1,25 @@
+import { CommentBody, CommentBox, CommentHeaderBox, TimeStampBox } from './style';
+import UserImageLarge from '@assets/images/UserImageLarge.svg';
+import Label from '@components/common/lable';
+
+const CommentHeader = ({ image, assignee, timeStamp }) => {
+  return (
+    <CommentHeaderBox>
+      <img src={image} alt="이미지" />
+      <span>{assignee}</span>
+      <TimeStampBox>{timeStamp} 전</TimeStampBox>
+      <Label title="작성자" bgColor="black" />
+    </CommentHeaderBox>
+  );
+};
+
+const DetailContent = () => {
+  return (
+    <CommentBox>
+      <CommentHeader image={UserImageLarge} assignee="daon" timeStamp="9분" />
+      <CommentBody>이슈 트래커 언제 끝나아</CommentBody>
+    </CommentBox>
+  );
+};
+
+export default DetailContent;
