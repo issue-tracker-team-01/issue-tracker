95eb8700db7344a5b75f211ce9748fe5797d6a2c
poco <godiva140@gmail.com>
2023-06-02 03:22:31 +0900

    feat: 이슈 작성 페이지 title 입력 기능 구현
    이슈 작성 페이지 title 입력 기능 구현
    
    Related to #109

--- fe/src/components/issueForm/issueFormMainContent/mainContentInputs/Inputs.jsx
+++ fe/src/components/issueForm/issueFormMainContent/mainContentInputs/Inputs.jsx
@@ -1,37 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
+import TextInputArea from './textInputArea';
+import TitleInputArea from './titleInput';
+import { IssueFormInfoStateContext } from '@pages/IssueForm';
 
 const InputsBox = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   width: 900px;
   height: 420px;
-  border: 1px solid red;
 `;
-
-const TitleInputBox = styled.div`
-
-  background-color:${({ theme }) => theme.COLOR.GRAY200}
-  border: 1px solid green;
-`;
-const CommentInputBox = styled.div`
-  background-color: ${({ theme }) => theme.COLOR.GRAY200};
-`;
-
-/* TODO: 입력창 스타일 추가 및 기능 구현 필요 */
 
 const Inputs = () => {
+  const { issueFormInfoState, dispatch } = useContext(IssueFormInfoStateContext);
   return (
     <InputsBox>
-      <TitleInputBox>
-        <label htmlFor="title">제목</label>
-        <input id="title" type="text" />
-      </TitleInputBox>
-      <CommentInputBox>
-        <label htmlFor="comment">코멘트를 입력하세요</label>
-        <textarea id="comment" type="text-area" />
-      </CommentInputBox>
-      <div></div>
+      <TitleInputArea issueFormInfoState={issueFormInfoState} dispatch={dispatch} />
+      <TextInputArea issueFormInfoState={issueFormInfoState} dispatch={dispatch} />
     </InputsBox>
   );
 };
