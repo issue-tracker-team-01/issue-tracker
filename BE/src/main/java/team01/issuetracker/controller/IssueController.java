329dbda197b8640167e19e8123aed9c7510fe8b1
jaea-kim <jaeakim98@gmail.com>
2023-06-01 14:54:28 +0900

    feat: swagger로 api 문서화 추가

--- BE/src/main/java/team01/issuetracker/controller/IssueController.java
+++ BE/src/main/java/team01/issuetracker/controller/IssueController.java
@@ -1,5 +1,7 @@
 package team01.issuetracker.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +11,7 @@ import team01.issuetracker.service.IssueService;
 import team01.issuetracker.service.dto.request.FilterRequestDTO;
 import team01.issuetracker.service.dto.request.IssueRequestDTO;
 
+@Tag(name = "Issue", description = "이슈 관련 API")
 @RequiredArgsConstructor
 @RequestMapping("/api/issues")
 @RestController
@@ -17,18 +20,21 @@ public class IssueController {
     private final IssueService issueService;
     private final Logger logger = LoggerFactory.getLogger(IssueController.class);
 
+    @Operation(summary = "이슈 목록 조회")
     @GetMapping
     public ResponseEntity<?> view(FilterRequestDTO requestDTO) {
         logger.info("이슈 필터 호출");
         return ResponseEntity.ok(issueService.getIssues(requestDTO));
     }
 
+    @Operation(summary = "새로운 이슈 작성")
     @PostMapping
     public void create(@RequestBody IssueRequestDTO issueDTO) {
         logger.debug("이슈 생성");
         issueService.create(issueDTO);
     }
 
+    @Operation(summary = "이슈 상세 보기")
     @GetMapping("/{id}")
     public ResponseEntity<?> detailView(@PathVariable Long id) {
         logger.debug("이슈 상세보기");
--- BE/src/main/java/team01/issuetracker/controller/MilestoneController.java
+++ BE/src/main/java/team01/issuetracker/controller/MilestoneController.java
@@ -1,5 +1,7 @@
 package team01.issuetracker.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import team01.issuetracker.service.MilestoneService;
 import team01.issuetracker.service.dto.response.MilestoneDTO;
 
+@Tag(name = "Milestone", description = "마일스톤 관련 API")
 @RequiredArgsConstructor
 @RequestMapping("/api/milestones")
 @RestController
@@ -16,30 +19,35 @@ public class MilestoneController {
     private final MilestoneService milestoneService;
     private final Logger logger = LoggerFactory.getLogger(MilestoneController.class);
 
+    @Operation(summary = "마일스톤 목록 조회")
     @GetMapping
     public ResponseEntity<?> milestoneView(@RequestParam("isOpen") boolean status) {
         logger.info(status + " 마일스톤 조회");
         return ResponseEntity.ok(milestoneService.getMilestone(status));
     }
 
+    @Operation(summary = "새로운 마일스톤 작성")
     @PostMapping
     public void create(@RequestBody MilestoneDTO milestoneDTO) {
         logger.debug("마일스톤 생성");
         milestoneService.create(milestoneDTO);
     }
 
+    @Operation(summary = "마일스톤 편집")
     @PatchMapping("/{milestoneId}")
     public void update(@RequestBody MilestoneDTO milestoneDTO, @PathVariable Long milestoneId) {
         logger.debug("마일스톤 편집");
         milestoneService.update(milestoneDTO, milestoneId);
     }
 
+    @Operation(summary = "마일스톤 상태 변경")
     @PatchMapping("/{milestoneId}/status")
     public void stateChanges(@PathVariable Long milestoneId) {
         logger.debug("마일스톤 상태 반전");
         milestoneService.stateChanges(milestoneId);
     }
 
+    @Operation(summary = "마일스톤 삭제")
     @DeleteMapping("/{milestoneId}")
     public void delete(@PathVariable Long milestoneId) {
         logger.debug("마일스톤 삭제");
